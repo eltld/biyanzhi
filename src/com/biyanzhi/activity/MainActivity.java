82e3eea75a7d441950a33c9754a94faeb3c7a22e
binbin <774663576@qq.com>
2015-05-08 11:52:13 +0800

    first commit

---
+++ src/com/biyanzhi/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.biyanzhi.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.biyanzhi.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
